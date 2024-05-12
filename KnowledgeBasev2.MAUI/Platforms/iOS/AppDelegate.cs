ca929fb4d480eff798a8fb32bc4b61a0a9591884
Waldheimer <dewaldheimer@googlemail.com>
2024-05-12 03:46:39 +0200

    Project Init

---
+++ KnowledgeBasev2.MAUI/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace KnowledgeBasev2.MAUI
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
